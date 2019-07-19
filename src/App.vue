e641cf298618ccac3812a580c80ff792eec43d8f
Vojislav <vojislav.stupar@gmail.com>
2019-07-19 19:13:28 +0200

    trivia-app route

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>TRIVIA-APP</h3>
+    <nav class="navbar navbar-brand"> 
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +16,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <TriviaApp msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TriviaApp from '@/components/TriviaApp.vue'
+
+export default {
+  name: 'home',
+  components: {
+    TriviaApp
+  }
+}
+</script>
